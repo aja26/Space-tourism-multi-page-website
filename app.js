ccc7e4856d8985c450cb3edd6bb0ec634c377df7
Adam Alexander <46898882+aja26@users.noreply.github.com>
2022-10-29 23:25:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,37 @@ navBut.addEventListener('click', () => {
 // Tabs
 
 const tabList = document.querySelector('[role="tablist"]');
-const tabs = tabList.querySelector('[role="tab"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+let tabFocus = 0;
+ console.log(tabList, tabs);
 
-console.log(tabList, tabs);
+tabList.addEventListener('keydown',  changeTabFocus)
+
+
+
+function changeTabFocus(e){
+    const keyDownLeft = 37;
+    const keyDownRight = 39;
+
+    // change tab index of the current tab to -1
+    if (e.keyCode === keyDownLeft || e.keyCode === keyDownRight){
+    tabs[tabFocus].setAttribute('tabindex', -1)
+    }
+
+    if (e.keyCode === keyDownRight){
+        tabFocus ++;   
+        if(tabFocus >= tabs.length){
+            tabFocus = 0;
+        }
+    }
+
+    if (e.keyCode === keyDownLeft){
+        tabFocus --;
+        if(tabFocus < 0){
+            tabFocus = tabs.length -1;
+        }
+    }
+
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
+}
